8f952273c58793a1a0d5505ebf0d71e61b78de8f
daniel-monsalve-villegas <unbigote@hush.com>
2022-10-21 11:52:54 -0400

    solucionado ejercicio 5

---
+++ 05-filtered-ls.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const fs = require ('fs');
+const path = require('path');
+
+const ext = '.' + process.argv[3];
+
+fs.readdir(process.argv[2], function(err, list) {
+	if(err) {
+		console.log(err);
+	}
+	list.forEach(file => {
+		if(path.extname(file) == ext)
+			console.log(file);
+	})
+})
