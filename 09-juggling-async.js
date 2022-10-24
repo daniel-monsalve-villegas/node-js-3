174988b5a9ee8898429728228064b13cecebff6b
daniel-monsalve-villegas <unbigote@hush.com>
2022-10-24 11:23:23 -0400

    solucionado ejercicio 9

---
+++ 09-juggling-async.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const http = require('http');
+const bl = require('bl');
+const urls = process.argv.slice(2);
+
+http.get(urls[0], response => {
+	response.pipe(bl((err, data) => {
+		if(err) return console.log(err);
+		data = data.toString()
+		console.log(data);
+
+		http.get(urls[1], response => {
+			response.pipe(bl((err, data) => {
+				if(err) return console.log(err);
+				data = data.toString();
+				console.log(data);
+
+				http.get(urls[2], response => {
+					response.pipe(bl((err, data) => {
+						if(err) return console.log(err);
+						data = data.toString();
+						console.log(data);
+					}));
+				});
+			}));
+		});
+	}));
+});
